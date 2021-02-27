4e3209360bf109b9482bb7c797b57e04b0c444da
sele14 <eirik.selander@gmail.com>
2021-02-27 17:13:08 +0000

    first commit

---
+++ code_snippets.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+inline void keep_window_open() { char ch; cin>>ch; }
+
+/*
+ Code snippet 1: Hello world
+    -> this program outputs the message "Hello, world!"
+*/
+
+// int main()
+// {
+//     cout << "Hello world! \n";
+
+//     return 0;
+// }
+
+///////
+
+// Code snippet 2: Drill 1
+
+// int main()
+// {
+//     cout << "Hello programming!\n";
+//     cout << "Here we go!\n";
+
+//     return 0;
+// }
+
+
+/*
+Code snippet 3: Input/output
+    - This function takes a string input and outputs hello + string.
+*/
+int main()
+{
+    cout << "Enter your name: \n";
+    string first_name;
+    cin >> first_name;
+    cout << "Hello, " << first_name << ".\n";
+
+}
